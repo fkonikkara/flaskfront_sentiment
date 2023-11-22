caf771811a392c3635a2224e062564bb2c757de3
fkonikkara <151709876+fkonikkara@users.noreply.github.com>
2023-11-22 12:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import joblib
 
 nltk.download('vader_lexicon')
 
 app = Flask(__name__)
-
-# Load your trained model
-model = joblib.load('path_to_your_model_file')  # Replace with your model's file path
+sid = SentimentIntensityAnalyzer()
 
 @app.route('/')
 def index():
@@ -18,10 +15,17 @@ def index():
 def analyze():
     if request.method == 'POST':
         review = request.form['review']
-        # Use your trained model for prediction
-        prediction = model.predict([review])  # Replace with your model's prediction method
-        # Process prediction and return result to template
-        return render_template('result.html', prediction=prediction)
+        scores = sid.polarity_scores(review)
+        sentiment = get_sentiment(scores)
+        return render_template('result.html', review=review, sentiment=sentiment)
+
+def get_sentiment(scores):
+    if scores['compound'] >= 0.05:
+        return 'Positive'
+    elif scores['compound'] <= -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
 
 if __name__ == '__main__':
     app.run(debug=True)
